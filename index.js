08711213c2272d57dccf6e5aff191a1343e1b053
Vaibhav Deshmukh <deshmukhvaibhav499@gmail.com>
2024-02-05 15:26:32 +0530

    some changes

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Db connection successfull");
 }).catch((error)=>console.log('error :>> ', error.message))
 
-const server = app.listen(process.env.PORT || 6010,()=>{
-    console.log("Server Listening on port: ",process.env.PORT);
+const server = app.listen(process.env.PORT || 5000,()=>{
+    console.log("Server Listening on port: ",process.env.PORT||5000);
 })
 
 const io = socket(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"http://localhost:3000" || process.env.FRONTEND,
         credential: true
     }
 });
